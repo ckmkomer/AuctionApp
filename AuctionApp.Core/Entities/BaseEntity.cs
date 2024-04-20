d96e486eff18c404828587c837705cb51aa9c584
Ömer Çakmak <ckmkomr1@gmail.com>
2024-04-20 18:28:56 +0300

    Add project files.

---
+++ AuctionApp.Core/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionApp.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }= DateTime.Now;
+        public DateTime UpdateDate { get; set; }
+        public DateTime DeletionDate { get; set; }
+    }
+}
---
+++ AuctionApp.Data/Context/AuctionAppDb.cs
@@ -0,0 +1,22 @@
+﻿using AuctionApp.Core.Entities;
+using AuctionApp.Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionApp.Data.Context
+{
+    public class AuctionAppDb : IdentityDbContext<AppUser>
+    {
+        public AuctionAppDb(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet< Vehicle> Vehicles {  get; set; }    
+    }
+}
